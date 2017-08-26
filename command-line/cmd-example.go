7b2f134961ed277fc9748679e0c33a217d2a9153
Will Liu <William.Q.Liu@gmail.com>
2017-08-26 08:30:31 -0600

    Comments about Command Line Arguments

--- command-line/cmd-example.go
+++ command-line/cmd-example.go
@@ -4,12 +4,15 @@ import "os"
 import "fmt"
 
 func main() {
+
+	// Command-Line Arguments used to parameterize execution of programs
+	// e.g. `go run hello.go` uses `run` and `hello.go` arguments to the `go` program
 	argsWithProg := os.Args
 	argsWithoutProg := os.Args[1:]
-
 	arg := os.Args[3]
 
-	fmt.Println(argsWithProg)
-	fmt.Println(argsWithoutProg)
-	fmt.Println(arg)
+	// Usage: `cmd-example.go a b c d e`
+	fmt.Println(argsWithProg)    // [./cmd-example a b c d e]
+	fmt.Println(argsWithoutProg) // [a b c d e]
+	fmt.Println(arg)             // c
 }
